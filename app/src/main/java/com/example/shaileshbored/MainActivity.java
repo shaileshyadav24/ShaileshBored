85d87b61a86ae1331675784cd286fbf9b5076c42
Shailesh Yadav <shaileshkyadav24@gmail.com>
2021-02-09 06:05:46 +0530

    Fetch new activity when clicked on "Show another activity"

--- app/src/main/java/com/example/shaileshbored/MainActivity.java
+++ app/src/main/java/com/example/shaileshbored/MainActivity.java
@@ -5,15 +5,19 @@ import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.shaileshbored.ViewModel.BoredViewModel;
 import com.example.shaileshbored.model.Bored;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private BoredViewModel boredViewModel;
     private TextView tvActivity;
+    private Button btnAddToFav;
+    private Button btnShowAnother;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +29,34 @@ public class MainActivity extends AppCompatActivity {
         boredViewModel.fetchAllData();
 
         this.tvActivity = findViewById(R.id.tvActivity);
-
+        this.btnAddToFav = findViewById(R.id.addToFav);
+        this.btnAddToFav.setOnClickListener(this);
+        this.btnShowAnother = findViewById(R.id.btnShowAnother);
+        this.btnShowAnother.setOnClickListener(this);
 
         this.boredViewModel.boredMutableLiveData.observe(this, new Observer<Bored>() {
             @Override
             public void onChanged(Bored bored) {
-                if(bored != null) {
-                    tvActivity.setText(bored.getActivity().toString());
+                if (bored != null) {
+                    tvActivity.setText(bored.getActivity());
                 }
             }
         });
 
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view != null) {
+            switch (view.getId()) {
+                case R.id.addToFav:
+                    // firestore
+                    break;
+                case R.id.btnShowAnother:
+                    boredViewModel.fetchAllData();
+                    break;
+            }
+        }
     }
 }
